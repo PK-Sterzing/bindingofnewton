b9ed14707452e7bcdb236f55a395dfd6abfabbd1
kaffarell <gabrielgoller123@gmail.com>
2021-04-04 15:04:08 +0200

    First commit

---
+++ desktop/src/com/bindingofnewton/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bindingofnewton.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bindingofnewton.game.BindingOfNewton;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BindingOfNewton(), config);
+	}
+}
