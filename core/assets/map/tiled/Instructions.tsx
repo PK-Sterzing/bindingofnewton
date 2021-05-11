f6e9ac2cb6025a112001a0b97e03e52e760fcedc
Getter44 <max.molling@gmail.com>
2021-05-11 11:20:12 +0200

    added several rooms, overworked main menu

---
+++ core/assets/map/tiled/Instructions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Instructions" tilewidth="48" tileheight="48" tilecount="1" columns="1">
+ <image source="../../instruction_shoot.png" width="48" height="48"/>
+</tileset>
