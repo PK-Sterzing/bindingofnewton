dce3d612d99c00aa2e0a00265bfb5ca767ddd65d
lober <l.oberhauser02@gmail.com>
2021-05-18 16:12:23 +0200

    Added SoundHandler and MainMenuMusic

--- core/src/com/bindingofnewton/game/BindingOfNewtonGame.java
+++ core/src/com/bindingofnewton/game/BindingOfNewtonGame.java
@@ -1,10 +1,12 @@
 package com.bindingofnewton.game;
 
 import com.badlogic.gdx.Game;
+import com.bindingofnewton.game.assets.SoundHandler;
 
 public class BindingOfNewtonGame extends Game {
     @Override
     public void create() {
+        SoundHandler.getInstance().playMusic(SoundHandler.Music.MAIN_MENU, true);
         setScreen(new MainMenu(this));
     }
 }
