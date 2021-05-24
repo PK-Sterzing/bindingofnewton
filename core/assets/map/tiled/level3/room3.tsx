763ece1f2ca4d70cc60c29b95ded9388d8f8788b
Getmackt <g.snakednet@gmail.com>
2021-05-24 13:19:21 +0200

    First iteration of rooms for 3rd floor

--- core/assets/map/tiled/level3/room3.tsx
+++ core/assets/map/tiled/level3/room3.tsx
@@ -1,4 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="room" tilewidth="0" tileheight="0" tilecount="0" columns="0">
+<tileset version="1.5" tiledversion="1.6.0" name="room" tilewidth="32" tileheight="48" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../img/lava-corner.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../img/lava-edge.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../img/lava-floor.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="48" source="../../img/fire1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="48" source="../../img/fire2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="48" source="../../img/fire3.png"/>
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
